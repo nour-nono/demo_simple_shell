8808aeca7ec33439136ed1ded64ebd275102df8c
nono <nour98nour@gmail.com>
2023-10-12 23:12:20 +0300

    edited 1 file

--- is_interactive.c
+++ is_interactive.c
@@ -9,9 +9,10 @@ void is_interactive()
         ssize_t sz = 0;
         printf("%s", prompt);
         getline(&buff, &sz, stdin);
-        char **arr = **make_arr_of_str(buff, " ")
+        char **arr = make_arr_of_str(buff, " ");
         printf("%s", buff);
-        free(buff)
+        free(arr);
+        free(buff);
     }
 }
 
