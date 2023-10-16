14d854c70df4a12753b5fc2f76e2c89e2f6124e3
ahmedemc2 <50429714+ahmedemc2@users.noreply.github.com>
2023-10-16 15:18:28 +0100

    Create implimented_functions1.c

---
+++ implimented_functions1.c
@@ -0,0 +1,37 @@
+/**
+ * print_env - print environment
+ *
+ * Description: function that print the environment
+ *
+ * Return: nothing
+ */
+void print_env(void)
+{
+  int i = 0;
+  char c = '\n';
+
+  while (*(environ + i) != NULL)
+  {
+    write(1, *(environ + i), strlen(*(environ + i)));
+    write(1, &c, 1);
+    i++;
+  }
+}
+/**
+ * is_exit - out if is exit
+ *
+ * @arr: is a pointer
+ *
+ * Description: this program prints "_putchar"
+ *
+ * Return: nothing
+ */
+
+void is_exit(char ***arr)
+{
+  int i = 0;
+  
+  i = atoi((*arr)[1]);
+  free_array(&arr);
+  exit(i);
+}
