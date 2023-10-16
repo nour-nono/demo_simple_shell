b35a0975dad27e6cdf6936dd68fae0b01ca3ed20
nour-nono <115364839+nour-nono@users.noreply.github.com>
2023-10-16 17:18:56 +0300

    Update string_funcs2.c

--- string_funcs2.c
+++ string_funcs2.c
@@ -87,3 +87,46 @@ char *_strdup(char *str)
 	}
 	return (ch);
 }
+
+/**
+ * _atoi - Entry point
+ *
+ * @s: is a pointer
+ *
+ * Description: this program prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+int _atoi(char *s)
+{
+	int i = 0;
+	unsigned int num = 0;
+	int cnt = 0;
+	int flag = 0;
+
+	if (!s)
+		return (0);
+	while (s[i] != '\0' && (s[i] > '9' || s[i] < '0'))
+	{
+		if (s[i] == '-')
+		++cnt;
+		++i;
+	}
+	while (s[i] != '\0' && s[i] <= '9' && s[i] >= '0')
+	{
+		num += s[i] - '0';
+		flag = 0;
+		if (num < (INT_MAX  / 10 + 2))
+		{
+			num *= 10;
+			flag = 1;
+		}
+		++i;
+	}
+	if (flag)
+		num /= 10;
+	if (cnt % 2)
+		return (-num);
+	else
+		return (num);
+}
